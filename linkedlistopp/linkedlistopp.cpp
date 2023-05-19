c519146d203d68261e035935d251f39498487be8
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-05-19 16:23:54 +0700

    membuat perulangan while dan set ulang previous dan set ulang current. diluar loop set setelah nodeBaru menjadi current dan setelah previous menjadi nodeBru

--- linkedlistopp/linkedlistopp.cpp
+++ linkedlistopp/linkedlistopp.cpp
@@ -44,5 +44,21 @@ void List::addNode() {/*Menambahkan sebuah node kedalam list*/
 		START = nodeBaru;
 		return;
 	}
+	Node* previous, * current;
 
+	current = START;
+	previous = START;
+
+	while ((current != NULL) && (nim >= current->noMhs)) {
+		if (nim = current->noMhs) {
+			cout << "\nDuplikasi noMhs tidak diijinkan\n";
+			return;
+		}
+		previous = current;
+		current = current->next;
+
+	}
+	/*Jika loop diatas dieksekusi, previous dan current akan menempati posisi dimana */
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
